813b84fd0fe791090efa75c4d1f9db3cdeff3708
jmmg0929 <54376183+jmmg0929@users.noreply.github.com>
2019-11-13 12:45:17 -0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+// in your terminal, write > cp config.default.js config.js and change the following values
+// get these keys in https://apps.twitter.com/app/new 
+
+module.exports = {
+  consumer_key: 'oSSp9IndOC5cVEApL4YHBucgS',
+  consumer_secret: 'n1sTbElCeqszDE7HFnYoKQEG0bmMiNk7WQya1fqhRMObdh7n98',
+  access_token_key: '828379020542763010-GX8VStxZJviv2u2QMCJNx8L7FJC4ATJ',
+  access_token_secret: 'o7drGsbpBQcYg13SD0kfMSMuA2WWGNvPJNPobuw4hWeWV'
+}
---
+++ get-tweets.js
@@ -0,0 +1,56 @@
+const Twitter = require('twitter');
+const config = require('./config.js');
+const fs = require('fs');
+const T = new Twitter(config);
+
+console.log("Launching twitter-bot script");
+
+// Set up your search parameters
+const params = {
+  q: '#greatbritishbakeoff',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+// Initiate your search using the above paramaters
+T.get('search/tweets', params, (err, data, response) => {
+  // If there is no error, proceed
+  if(err){
+    return console.log(err);
+  }
+
+  // Loop through the returned tweets
+  const tweetsId = data.statuses
+    .map(tweet => ({ id: tweet.id_str }));
+    // console.log(tweet[0].text)
+
+
+    var tweets = [];
+    for (var i =0; i < data.statuses.length; i ++){
+      // console.log(data.statuses[i].text);
+      tweets.push({id: data.statuses[i].id_str, text: data.statuses[i].text, });
+      // var id ={id: data.statuses[i].id_str}
+    }
+    console.log(tweets);
+    var completeData = JSON.stringify(tweets);
+    fs.writeFileSync('tweets.json', completeData);
+
+    for(var i = 0; i < data.statuses.length; i ++){
+
+    }
+    console.log(completeData);
+    console.log("----- saved as tweets.json -----")
+  // tweetsId.map(tweetId => {
+  //   T.post('favorites/create', tweetId, (err, response) => {
+  //     if(err){
+  //       return console.log(err[0].message);
+  //     }
+
+  //     const username = response.user.screen_name;
+  //     const favoritedTweetId = response.id_str;
+  //     console.log(`Favorited: https://twitter.com/${username}/status/${favoritedTweetId}`);
+
+  //   });
+  // });
+})
